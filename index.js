a76618819cd1cb3eb0b53ab3eaaf49896220ce9a
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-05-21 10:52:26 +0300

    Add stopPropagation

--- index.js
+++ index.js
@@ -7,15 +7,17 @@ const section = document.querySelector('section');
 unique.addEventListener('click', handlerClick);
 // об'єктна властивість capture:true - вказує на те, що обробник відпрацює на етапі занурення
 // об'єктна властивість once:true - вказує на те, що обробник відпрацює лише ОДИН раз
-window.addEventListener('click', handlerClick, {capture:true, once:true});
+window.addEventListener('click', handlerClick);
 // третій параметр true - вказує на те, що обробник відпрацює на етапі занурення
-main.addEventListener('click', handlerClick, true);
+main.addEventListener('click', handlerClick);
 section.addEventListener('click', handlerClick);
 //об'єктна властивість once:true - використовується для анонімних функцій
-document.addEventListener('click', ()=>{console.log(123);}, {once:true});
+document.addEventListener('click', (event)=>{event.stopPropagation();console.log(123);}, {once:true});
 document.body.addEventListener('click', handlerClick, {once:true});
 
 function handlerClick(event) {
+  //виконує подію тільки на одному обробнику
+  event.stopPropagation();
   console.group();
   //console.log(event); // об'єкт події який згенерував браузер
   console.dir(event.currentTarget); //чий обробник спрацював
