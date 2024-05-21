a4a5094dbb243e7813b10201f8c9c794629831ba
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-05-21 10:42:08 +0300

    Add event capture & once

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 'use strict';
 
 const unique = document.getElementById('unique');
+const main = document.querySelector('main');
+const section = document.querySelector('section');
 
-//генеруємо об'єкт власної події
-const objMyEvent = new Event('myEvent');
+unique.addEventListener('click', handlerClick);
+// об'єктна властивість capture:true - вказує на те, що обробник відпрацює на етапі занурення
+// об'єктна властивість once:true - вказує на те, що обробник відпрацює лише ОДИН раз
+window.addEventListener('click', handlerClick, {capture:true, once:true});
+// третій параметр true - вказує на те, що обробник відпрацює на етапі занурення
+main.addEventListener('click', handlerClick, true);
+section.addEventListener('click', handlerClick);
+//об'єктна властивість once:true - використовується для анонімних функцій
+document.addEventListener('click', ()=>{console.log(123);}, {once:true});
+document.body.addEventListener('click', handlerClick, {once:true});
 
-unique.addEventListener('myEvent', handlerClick);
-
-function handlerClick() {
-  //alert(1)
-  console.log('handlerClick');
-  unique.removeEventListener('click', handlerClick);
-  unique.disabled = true;
-}
-
-if (Math.random() > 0.5) {
-  //викликаємо згенерований об'єкт події на елементі
-  unique.dispatchEvent(objMyEvent);
+function handlerClick(event) {
+  console.group();
+  //console.log(event); // об'єкт події який згенерував браузер
+  console.dir(event.currentTarget); //чий обробник спрацював
+  console.dir(event.target); //той по якому клікнули(той де згенерувалася подія)
+  console.groupEnd();
 }
