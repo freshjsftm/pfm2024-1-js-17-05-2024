de47665d8bbb57c31d6f2689f0fa52c4fccfcf49
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-05-21 12:28:26 +0300

    Add dataset

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
 'use strict';
-
 const unique = document.getElementById('unique');
-const main = document.querySelector('main');
+const [main] = document.getElementsByTagName('main');
 const section = document.querySelector('section');
-
-unique.addEventListener('click', handlerClick);
-// об'єктна властивість capture:true - вказує на те, що обробник відпрацює на етапі занурення
-// об'єктна властивість once:true - вказує на те, що обробник відпрацює лише ОДИН раз
-window.addEventListener('click', handlerClick);
-// третій параметр true - вказує на те, що обробник відпрацює на етапі занурення
-main.addEventListener('click', handlerClick);
-section.addEventListener('click', handlerClick);
-//об'єктна властивість once:true - використовується для анонімних функцій
-document.addEventListener('click', (event)=>{event.stopPropagation();console.log(123);}, {once:true});
-document.body.addEventListener('click', handlerClick, {once:true});
-
-function handlerClick(event) {
-  //виконує подію тільки на одному обробнику
-  event.stopPropagation();
-  console.group();
-  //console.log(event); // об'єкт події який згенерував браузер
-  console.dir(event.currentTarget); //чий обробник спрацював
-  console.dir(event.target); //той по якому клікнули(той де згенерувалася подія)
-  console.groupEnd();
+//перевіряємо наявність атрибуту
+if (main.hasAttribute('title')) {
+  // отримати значення атрибуту, якщо атрибуту не має поверне null
+  console.log(main.getAttribute('title'));
+  //перезаписуємо значення існуючого атрибуту
+  main.setAttribute('title', 'new value for main title');
+  //видалити атрибут
+  // main.removeAttribute('title');
 }
+console.log(section.hasAttribute('title'));
+// створюємо новий атрибут
+section.setAttribute('title', 'title for section');
+unique.setAttribute('title', 'title for unique');
+
+// create object Attr
+const attrId = document.createAttribute('id');
+console.dir(attrId);
+//встановлюю знаяення в об'єкт-атрибут
+attrId.value = 'ident-for-section';
+console.log(attrId);
+//додаю об'єкт-атрибут до елемента
+section.setAttributeNode(attrId);
+attrId.value = 'new-ident-for-section';
+
+// dataset -> DOMStringMap
+console.log(section.dataset);
+section.dataset.test = 'new data for data-test';
+console.log(section.dataset.test);
+console.log(section.dataset.countSome);
