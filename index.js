de9b24d8e1846b7f6381fd29af4793e44669164c
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-05-23 10:29:04 +0300

    Add task data-color

--- index.js
+++ index.js
@@ -1,33 +1,44 @@
 'use strict';
-const unique = document.getElementById('unique');
-const [main] = document.getElementsByTagName('main');
-const section = document.querySelector('section');
-//перевіряємо наявність атрибуту
-if (main.hasAttribute('title')) {
-  // отримати значення атрибуту, якщо атрибуту не має поверне null
-  console.log(main.getAttribute('title'));
-  //перезаписуємо значення існуючого атрибуту
-  main.setAttribute('title', 'new value for main title');
-  //видалити атрибут
-  // main.removeAttribute('title');
+// 1 variant
+// const btns1 = document.querySelectorAll('#wrapper-btn button');
+// console.log(btns1);
+
+// 2 variant
+//const wrapperBtn = document.getElementById('wrapper-btn');
+const btns2 = document.getElementById('wrapper-btn').children;
+console.log(btns2);
+
+// const handlerBtn = (event)=>{
+//   const elem = event.target;
+//   const parentElem = elem.parentElement;
+//   const color = elem.dataset.color;
+//   parentElem.style.setProperty('background-color', color);
+//   parentElem.style.setProperty('border-color', color);
+//   //wrapperBtn.style.setProperty('background-color', color);
+//   //wrapperBtn.style.backgroundColor = color;
+//   //wrapperBtn.style.borderColor = color;
+// }
+
+// const handlerBtn = (event)=>{
+//   const parentElem = event.target.parentElement;
+//   const color = event.target.dataset.color;
+//   parentElem.style.setProperty('background-color', color);
+//   parentElem.style.setProperty('border-color', color);
+// }
+
+const handlerBtn = ({
+  target: {
+    parentElement:{style:styleForParent},
+    dataset: { color },
+  },
+}) => {
+  styleForParent.setProperty('background-color', color);
+  styleForParent.setProperty('border-color', color);
+};
+
+for (const btn of btns2) {
+  btn.addEventListener('click', handlerBtn);
+  btn.addEventListener('click', ({target})=>{
+    console.dir(target);
+  }, {once:true});
 }
-console.log(section.hasAttribute('title'));
-// створюємо новий атрибут
-section.setAttribute('title', 'title for section');
-unique.setAttribute('title', 'title for unique');
-
-// create object Attr
-const attrId = document.createAttribute('id');
-console.dir(attrId);
-//встановлюю знаяення в об'єкт-атрибут
-attrId.value = 'ident-for-section';
-console.log(attrId);
-//додаю об'єкт-атрибут до елемента
-section.setAttributeNode(attrId);
-attrId.value = 'new-ident-for-section';
-
-// dataset -> DOMStringMap
-console.log(section.dataset);
-section.dataset.test = 'new data for data-test';
-console.log(section.dataset.test);
-console.log(section.dataset.countSome);
