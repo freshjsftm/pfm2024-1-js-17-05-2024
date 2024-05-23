be8b0ed381871c8304ba9baab23a4946dd7271a6
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-05-23 11:33:30 +0300

    Add classList

--- index.js
+++ index.js
@@ -1,30 +1,18 @@
 'use strict';
 
 const wrapperBtn = document.getElementById('wrapper-btn');
-const btn = document.querySelector('#wrapper-btn button');
-const closeBtn = document.getElementById('close');
+const { children } = wrapperBtn;
 
-closeBtn.addEventListener('click', (event) => {
-  //відмінити обробник за замовчуванням
-  event.preventDefault();
-  //дістаємо сусіда знизу
-  const { target } = event;
-  const isClose = target.innerText === 'close';
-  target.innerText = isClose ? 'open' : 'close';
-  //вирізаємо елемент зі сторінки назад не повернемо!!!
-  //target.nextElementSibling.remove();
-  const display = isClose ? 'none' : 'block';
-  target.nextElementSibling.style.setProperty('display', display);
-});
+//console.log(children);
 
-// при кліку на кнопку змінити текст on на off
-btn.addEventListener('click', ({ target }) => {
-  target.innerText = target.innerText === 'on' ? 'off' : 'on';
-});
-
-// console.dir(wrapperBtn);
-// console.log(wrapperBtn.innerHTML);
-// console.log(wrapperBtn.innerText);
-// console.log(wrapperBtn.outerHTML);
-// console.log(wrapperBtn.outerText);
-// console.log(wrapperBtn.textContent);
+for (const child of children) {
+  child.addEventListener('click', ({ target }) => {
+    //надає можливість працювати з класами
+    console.dir(child.classList);
+    //child.className += ' active'
+    child.classList.add('old');
+    child.classList.toggle('active');
+    child.classList.remove('heading');
+    console.dir(child.classList);
+  });
+}
