4bf21d9d5ec41ac5086629b94520873195e03315
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-05-23 11:16:01 +0300

    Add preventDefault

--- index.js
+++ index.js
@@ -1,44 +1,30 @@
 'use strict';
-// 1 variant
-// const btns1 = document.querySelectorAll('#wrapper-btn button');
-// console.log(btns1);
 
-// 2 variant
-//const wrapperBtn = document.getElementById('wrapper-btn');
-const btns2 = document.getElementById('wrapper-btn').children;
-console.log(btns2);
+const wrapperBtn = document.getElementById('wrapper-btn');
+const btn = document.querySelector('#wrapper-btn button');
+const closeBtn = document.getElementById('close');
 
-// const handlerBtn = (event)=>{
-//   const elem = event.target;
-//   const parentElem = elem.parentElement;
-//   const color = elem.dataset.color;
-//   parentElem.style.setProperty('background-color', color);
-//   parentElem.style.setProperty('border-color', color);
-//   //wrapperBtn.style.setProperty('background-color', color);
-//   //wrapperBtn.style.backgroundColor = color;
-//   //wrapperBtn.style.borderColor = color;
-// }
+closeBtn.addEventListener('click', (event) => {
+  //відмінити обробник за замовчуванням
+  event.preventDefault();
+  //дістаємо сусіда знизу
+  const { target } = event;
+  const isClose = target.innerText === 'close';
+  target.innerText = isClose ? 'open' : 'close';
+  //вирізаємо елемент зі сторінки назад не повернемо!!!
+  //target.nextElementSibling.remove();
+  const display = isClose ? 'none' : 'block';
+  target.nextElementSibling.style.setProperty('display', display);
+});
 
-// const handlerBtn = (event)=>{
-//   const parentElem = event.target.parentElement;
-//   const color = event.target.dataset.color;
-//   parentElem.style.setProperty('background-color', color);
-//   parentElem.style.setProperty('border-color', color);
-// }
+// при кліку на кнопку змінити текст on на off
+btn.addEventListener('click', ({ target }) => {
+  target.innerText = target.innerText === 'on' ? 'off' : 'on';
+});
 
-const handlerBtn = ({
-  target: {
-    parentElement:{style:styleForParent},
-    dataset: { color },
-  },
-}) => {
-  styleForParent.setProperty('background-color', color);
-  styleForParent.setProperty('border-color', color);
-};
-
-for (const btn of btns2) {
-  btn.addEventListener('click', handlerBtn);
-  btn.addEventListener('click', ({target})=>{
-    console.dir(target);
-  }, {once:true});
-}
+// console.dir(wrapperBtn);
+// console.log(wrapperBtn.innerHTML);
+// console.log(wrapperBtn.innerText);
+// console.log(wrapperBtn.outerHTML);
+// console.log(wrapperBtn.outerText);
+// console.log(wrapperBtn.textContent);
