36df4a94fa5e8cbd75359bd028a8be591bae3080
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-05-24 09:39:16 +0300

    Add wheel

--- index.js
+++ index.js
@@ -1,20 +1,41 @@
 'use strict';
 
-const { children: items } = document.getElementById('main-menu');
+// const { children: items } = document.getElementById('main-menu');
+// for (const item of items) {
+//   item.classList.add('item');
+//   let posY = 0;
+//   item.addEventListener('wheel', (event) => {
+//     //зупинили скролювання сторінки
+//     event.preventDefault();
 
+//     const [link] = event.currentTarget.children;
+//     if (event.deltaY > 0) {
+//       posY -= 3;
+//       link.style.setProperty('padding-left', posY + 'px');
+//     } else {
+//       posY += 3;
+//       link.style.setProperty('padding-left', posY + 'px');
+//     }
+//   });
+// }
+
+const handlerWheel = (posY = 0)=> (event) => {
+  event.preventDefault();
+
+  const [link] = event.currentTarget.children;
+  if (event.deltaY > 0) {
+    posY -= 3;
+    link.style.setProperty('padding-left', posY + 'px');
+  } else {
+    posY += 3;
+    link.style.setProperty('padding-left', posY + 'px');
+  }
+}
+
+const { children: items } = document.getElementById('main-menu');
 for (const item of items) {
   item.classList.add('item');
-  item.addEventListener('click', (e) => {
-    e.stopPropagation();
-    e.preventDefault();
-    for (const item of items) {
-      item.classList.remove('active');
-    }
-    e.currentTarget.classList.add('active');
-    // тільки на читання
-    // e.currentTarget.getBoundingClientRect().width = 100;
-    console.log(e.currentTarget.getBoundingClientRect());
-    const height = e.currentTarget.getBoundingClientRect().height;
-    e.currentTarget.style.setProperty('width', height+'px');
-  });
+  item.addEventListener('wheel', handlerWheel());
 }
+
+// scroll  -> wheel, рухом повзунка, відповідні кнопки на клавіатурі
