6406874c6e2ffb1cefb409e9f28898ac7a9234e3
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-05-24 11:45:03 +0300

    Add render actors list

--- index.js
+++ index.js
@@ -1,41 +1,56 @@
-'use strict';
+'use script';
+console.table(actors);
+const root = document.getElementById('root');
 
-// const { children: items } = document.getElementById('main-menu');
-// for (const item of items) {
-//   item.classList.add('item');
-//   let posY = 0;
-//   item.addEventListener('wheel', (event) => {
-//     //зупинили скролювання сторінки
-//     event.preventDefault();
+const section = document.createElement('section');
+section.classList.add('actors-section');
 
-//     const [link] = event.currentTarget.children;
-//     if (event.deltaY > 0) {
-//       posY -= 3;
-//       link.style.setProperty('padding-left', posY + 'px');
-//     } else {
-//       posY += 3;
-//       link.style.setProperty('padding-left', posY + 'px');
-//     }
-//   });
-// }
+const h1 = document.createElement('h1');
+h1.classList.add('main-title');
+h1.innerText = 'actors';
 
-const handlerWheel = (posY = 0)=> (event) => {
-  event.preventDefault();
+const ul = document.createElement('ul');
+ul.classList.add('actors-list');
 
-  const [link] = event.currentTarget.children;
-  if (event.deltaY > 0) {
-    posY -= 3;
-    link.style.setProperty('padding-left', posY + 'px');
-  } else {
-    posY += 3;
-    link.style.setProperty('padding-left', posY + 'px');
-  }
-}
+const items = actors.map((actor) => {
+  const { name, photo, birthdate } = actor;
 
-const { children: items } = document.getElementById('main-menu');
-for (const item of items) {
-  item.classList.add('item');
-  item.addEventListener('wheel', handlerWheel());
-}
+  const item = document.createElement('li');
+  item.classList.add('actor-item');
 
-// scroll  -> wheel, рухом повзунка, відповідні кнопки на клавіатурі
+  const article = document.createElement('article');
+  article.classList.add('actor-card');
+  article.addEventListener('mouseenter', ({ target }) => {
+    target.style.setProperty('background-color', '#eee');
+  });
+  article.addEventListener('mouseleave', ({ target }) => {
+    target.style.setProperty('background-color', '');
+  });
+
+  const div = document.createElement('div');
+  div.classList.add('actor-pic-wrapper');
+
+  const img = document.createElement('img');
+  img.classList.add('actor-pic');
+  img.setAttribute('src', photo);
+  img.setAttribute('alt', name);
+
+  const h2 = document.createElement('h2');
+  h2.classList.add('actor-name');
+  // h2.innerText = name;
+  h2.append(name);
+
+  const p = document.createElement('p');
+  p.classList.add('actor-bd');
+  // p.innerText = birthdate;
+  p.append(birthdate);
+
+  div.append(img);
+  article.append(div, h2, p);
+  item.append(article);
+  return item;
+});
+
+ul.append(...items);
+section.append(h1, ul);
+root.append(section);
