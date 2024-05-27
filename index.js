cd20c20a6db42e2746cd709b44ffd73abce11b52
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-05-27 11:17:35 +0300

    Add input & change

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 'use strict';
 
-const {form} = document.forms;
-const {login} =form.elements;
+const { form } = document.forms;
+const { login } = form.elements;
 
 // click focus blur mouseenter mouseleave
 // keydown keyup keypress
-// ochange
-// paste copy cut 
-// select 
-login.addEventListener('keypress', (event)=>{
-  console.log('keypress');
-  console.log(event.key); //назву клавіші(символа) на який натисли
-  console.log(event.keyCode); //код клавіші(символа) на який натисли
+// input change
+// paste copy cut
+// select  invalid
+
+login.focus();
+login.select();
+
+login.addEventListener('input', (event) => {
+  console.log('input');
   console.log(event.target.value);
   console.log(event.target.nextElementSibling);
-  if(event.keyCode === 13){
-    if(event.target.value.length >= 3){
-      event.target.nextElementSibling.disabled = false;
-    }
+  if (event.target.value.length >= 3) {
+    event.target.nextElementSibling.disabled = false;
   }
-})
-login.addEventListener('blur', ({target})=>{
-  console.log('blur');
+});
+login.addEventListener('change', ({target})=>{
+  console.log('change');
   console.log(target.value);
   if(target.value.length >= 3){
     target.nextElementSibling.disabled = false;
@@ -29,7 +29,7 @@ login.addEventListener('blur', ({target})=>{
 })
 
 // у елемента форми є 2 події:  submit  reset
-form.addEventListener('submit', (event)=>{
+form.addEventListener('submit', (event) => {
   event.preventDefault();
   console.dir(event.target);
-})
+});
