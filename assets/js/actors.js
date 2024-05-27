5b8f2e0892651d6a8a161ef198c3ff29c0c96d52
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-05-27 10:05:14 +0300

    Add event click at submit element

--- assets/js/actors.js
+++ assets/js/actors.js
@@ -1,3 +1,5 @@
+'use script';
+
 const actors = [
   {
     id: 1,
@@ -48,3 +50,87 @@ const actors = [
     photo: '',
   },
 ];
+
+const stringToColour = (str) => {
+  let hash = 0;
+  str.split('').forEach((char) => {
+    hash = char.charCodeAt(0) + ((hash << 5) - hash);
+  });
+  let colour = '#';
+  for (let i = 0; i < 3; i++) {
+    const value = (hash >> (i * 8)) & 0xff;
+    colour += value.toString(16).padStart(2, '0');
+  }
+  return colour;
+};
+
+console.table(actors);
+const root = document.getElementById('root');
+
+const section = document.createElement('section');
+section.classList.add('actors-section');
+
+const h1 = document.createElement('h1');
+h1.classList.add('main-title');
+h1.innerText = 'actors';
+
+const ul = document.createElement('ul');
+ul.classList.add('actors-list');
+
+const items = actors
+  .filter((actor) => actor.name && actor.photo && actor.birthdate)
+  .map((actor) => {
+    const { name, photo, birthdate } = actor;
+
+    const item = document.createElement('li');
+    item.classList.add('actor-item');
+
+    const article = document.createElement('article');
+    article.classList.add('actor-card');
+    article.addEventListener('mouseenter', ({ target }) => {
+      target.style.setProperty('background-color', '#eee');
+    });
+    article.addEventListener('mouseleave', ({ target }) => {
+      target.style.setProperty('background-color', '');
+    });
+
+    const div = document.createElement('div');
+    div.classList.add('actor-pic-wrapper');
+
+    const span = document.createElement('span');
+    span.classList.add('actor-initial');
+    // отримати ініціали з name
+    span.append(name.at(0));
+    //stringToColour
+    span.style.setProperty('background-color', stringToColour(name));
+    div.append(span);
+
+    const img = document.createElement('img');
+    img.classList.add('actor-pic');
+    img.setAttribute('src', photo);
+    img.setAttribute('alt', name);
+    img.addEventListener('error', ({ target }) => {
+      target.remove();
+    });
+    img.addEventListener('load', ({ target }) => {
+      div.append(target);
+    });
+
+    const h2 = document.createElement('h2');
+    h2.classList.add('actor-name');
+    // h2.innerText = name;
+    h2.append(name ? name : '...');
+
+    const p = document.createElement('p');
+    p.classList.add('actor-bd');
+    // p.innerText = birthdate;
+    p.append(birthdate ? birthdate : '...');
+
+    article.append(div, h2, p);
+    item.append(article);
+    return item;
+  });
+
+ul.append(...items);
+section.append(h1, ul);
+root.append(section);
--- index.js
+++ index.js
@@ -1,85 +1,26 @@
-'use script';
+'use strict';
 
-const stringToColour = (str) => {
-  let hash = 0;
-  str.split('').forEach((char) => {
-    hash = char.charCodeAt(0) + ((hash << 5) - hash);
-  });
-  let colour = '#';
-  for (let i = 0; i < 3; i++) {
-    const value = (hash >> (i * 8)) & 0xff;
-    colour += value.toString(16).padStart(2, '0');
-  }
-  return colour;
-};
+// const form1v = document.getElementById('form');
+// console.dir(form1v);
 
-console.table(actors);
-const root = document.getElementById('root');
+//прямий доступ до усіх форм на сторінці
+const form2v = document.forms.form;
+// console.dir(form2v);
+//прямий доступ до всіх інтерактивних елементів форми
+// console.dir(form2v.elements);
+// console.dir(form2v.elements.login);
 
-const section = document.createElement('section');
-section.classList.add('actors-section');
+const {login, submit} = form2v.elements;
+// console.log(submit);
+// console.log(login);
 
-const h1 = document.createElement('h1');
-h1.classList.add('main-title');
-h1.innerText = 'actors';
+submit.addEventListener('click', (event)=>{
+  //event.preventDefault();
+  console.log(event.type);
+})
 
-const ul = document.createElement('ul');
-ul.classList.add('actors-list');
-
-const items = actors
-  .filter((actor) => actor.name && actor.photo && actor.birthdate)
-  .map((actor) => {
-    const { name, photo, birthdate } = actor;
-
-    const item = document.createElement('li');
-    item.classList.add('actor-item');
-
-    const article = document.createElement('article');
-    article.classList.add('actor-card');
-    article.addEventListener('mouseenter', ({ target }) => {
-      target.style.setProperty('background-color', '#eee');
-    });
-    article.addEventListener('mouseleave', ({ target }) => {
-      target.style.setProperty('background-color', '');
-    });
-
-    const div = document.createElement('div');
-    div.classList.add('actor-pic-wrapper');
-
-    const span = document.createElement('span');
-    span.classList.add('actor-initial');
-    // отримати ініціали з name
-    span.append(name.at(0));
-    //stringToColour
-    span.style.setProperty('background-color', stringToColour(name));
-    div.append(span);
-
-    const img = document.createElement('img');
-    img.classList.add('actor-pic');
-    img.setAttribute('src', photo);
-    img.setAttribute('alt', name);
-    img.addEventListener('error', ({ target }) => {
-      target.remove();
-    });
-    img.addEventListener('load', ({ target }) => {
-      div.append(target);
-    });
-
-    const h2 = document.createElement('h2');
-    h2.classList.add('actor-name');
-    // h2.innerText = name;
-    h2.append(name?name:'...');
-
-    const p = document.createElement('p');
-    p.classList.add('actor-bd');
-    // p.innerText = birthdate;
-    p.append(birthdate ? birthdate : '...');
-
-    article.append(div, h2, p);
-    item.append(article);
-    return item;
-  });
-
-ul.append(...items);
-section.append(h1, ul);
-root.append(section);
+// у елемента форми є 2 події:  submit  reset
+form2v.addEventListener('submit', (event)=>{
+  event.preventDefault();
+  console.log(event.type);
+})
