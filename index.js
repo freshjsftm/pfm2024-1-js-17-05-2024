d5d15964035845bd96f7746b0b1b714a815f11bf
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-05-27 10:39:31 +0300

    Add example handler events at input

--- index.js
+++ index.js
@@ -1,26 +1,35 @@
 'use strict';
 
-// const form1v = document.getElementById('form');
-// console.dir(form1v);
+const {form} = document.forms;
+const {login} =form.elements;
 
-//прямий доступ до усіх форм на сторінці
-const form2v = document.forms.form;
-// console.dir(form2v);
-//прямий доступ до всіх інтерактивних елементів форми
-// console.dir(form2v.elements);
-// console.dir(form2v.elements.login);
-
-const {login, submit} = form2v.elements;
-// console.log(submit);
-// console.log(login);
-
-submit.addEventListener('click', (event)=>{
-  //event.preventDefault();
-  console.log(event.type);
+// click focus blur mouseenter mouseleave
+// keydown keyup keypress
+// ochange
+// paste copy cut 
+// select 
+login.addEventListener('keypress', (event)=>{
+  console.log('keypress');
+  console.log(event.key); //назву клавіші(символа) на який натисли
+  console.log(event.keyCode); //код клавіші(символа) на який натисли
+  console.log(event.target.value);
+  console.log(event.target.nextElementSibling);
+  if(event.keyCode === 13){
+    if(event.target.value.length >= 3){
+      event.target.nextElementSibling.disabled = false;
+    }
+  }
+})
+login.addEventListener('blur', ({target})=>{
+  console.log('blur');
+  console.log(target.value);
+  if(target.value.length >= 3){
+    target.nextElementSibling.disabled = false;
+  }
 })
 
 // у елемента форми є 2 події:  submit  reset
-form2v.addEventListener('submit', (event)=>{
+form.addEventListener('submit', (event)=>{
   event.preventDefault();
-  console.log(event.type);
+  console.dir(event.target);
 })
