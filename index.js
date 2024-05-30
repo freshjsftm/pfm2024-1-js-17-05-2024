5c0237e9707aaccfa9014598b64db24d494be094
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-05-30 10:43:43 +0300

    Add example bad practice

--- index.js
+++ index.js
@@ -2,13 +2,30 @@
 
 console.log(1);
 
-const identificator = setInterval(() => {
-  console.log('subscribe');
-}, 3000);
+setTimeout(() => {
+  console.log('3');
+  console.time('time')
+  let summa = 0;
+  for(let i=0; i<100000;i++){
+    for(let j=0; j<100000;j++){
+      summa += i;
+    }
+  }
+  console.log(summa);
+  console.timeEnd('time')
+}, 100);
 
-document.getElementById('btn').addEventListener('click', () => {
-  clearInterval(identificator);
-  console.log('clear');
-});
+setTimeout(() => {
+  console.log('4');
+}, 100);
+
+setTimeout(() => {
+  console.log('5');
+   let summa = 0;
+  for(let i=0; i<1000;i++){
+    summa += i;
+  }
+  console.log(summa);
+}, 100);
 
 console.log(2);
