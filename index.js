7fbee8468606f52bdaf96d4326ff7e9c0a4ddf67
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-05-31 08:42:25 +0300

    Add tasks

--- index.js
+++ index.js
@@ -1,39 +1,27 @@
 'use strict';
 
-console.log(`
-написати функцію, яка буде послідовно в часі виводити числа від 1 до 10 з проміжком півсекунди
-`);
-console.log('start');
+const format = (value) => (value < 10 ? '0' + value : value);
+const root = document.getElementById('root');
 
-// function numbers() {
-//   let number = 1;
+console.log(`написати функцію, яка буде виводити годинник`);
+const paragraf = document.createElement('p');
+setInterval(() => {
+  const date = new Date();
+  paragraf.innerText = `${format(date.getHours())}:${format(
+    date.getMinutes()
+  )}:${format(date.getSeconds())}`;
+}, 100);
+root.append(paragraf);
 
-//   let id = setInterval(() => {
-//     console.log(number);
-//     number++;
-
-//     if (number > 10) {
-//       clearInterval(id);
-//     }
-//   }, 500);
-// }
-
-// numbers();
-
-function numbers(number = 1, max = 10, time = 500) {
-  return function () {
-    let id = setInterval(() => {
-      console.log(number);
-      number++;
-      for (let i = 0; i < 100000; i++) {}
-      for (let j = 0; j < 100000; j++) {}
-      if (number > max) {
-        clearInterval(id);
-      }
-    }, time);
-  };
-}
-console.time('counter');
-const counter = numbers(10, 15, 300);
-counter();
-console.timeEnd('counter');
+console.log(
+  `написати функцію, яка буде через 3 секунди виводити повідомлення на всю сторінку перекриваючи все, що міститься в body`
+);
+setTimeout(() => {
+  const fullscreen = document.createElement('div');
+  fullscreen.classList.add('fullscreen');
+  fullscreen.innerText = 'Ви ще тут ?'; 
+  root.append(fullscreen);
+  setTimeout(() => {
+    fullscreen.remove();
+  }, 5000);
+}, 3000);
