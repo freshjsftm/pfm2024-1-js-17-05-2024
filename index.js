709b43455734a78df6dc25f0947717bf705590c4
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-05-31 10:36:22 +0300

    Add example callback hell

--- index.js
+++ index.js
@@ -1,36 +1,26 @@
 'use strict';
 
-const user = {
-  fname:'Brad',
-  lname:'Pitt',
-  age:61,
-  getFullName(){
-    return this.fname + ' ' + this.lname;
-  },
-  isMale: true,
-  hasPet: undefined,
-  ukrainianPasport: null,
-  [Symbol('symbol')]:'symbol',
-  children:['alex','anna','max'],
-  address:{
-    town:'NY',
-    house:123,
-    flour: undefined,
-  },
-};
-
-console.log(user);
-const serialiseUser = JSON.stringify(user);
-console.log(typeof serialiseUser);
-console.log(serialiseUser);
+function f() {
+  console.log(1);
+  setTimeout(() => {
+    f();
+  }, 0);
+}
+// f();
 
 
-const deSerialiseUser = JSON.parse(serialiseUser);
-console.log(deSerialiseUser);
+// request - 30ms
 
-
-// Серіалізація — процес перетворення будь-якої структури даних у послідовність бітів. Зворотною до операції серіалізації є операція десеріалізації — відновлення початкового стану структури даних із бітової послідовності. Серіалізація використовується для передавання об'єктів мережею й для збереження їх у файлах.
-
-//JSON (англ. JavaScript Object Notation, укр. запис об'єктів JavaScript, вимовляється джéйсон[1]) — це текстовий формат обміну даними між комп'ютерами.
-
-
+function test(){
+  setTimeout(() => {
+    setTimeout(() => {
+      setTimeout(() => {
+        setTimeout(() => {
+          setTimeout(() => {
+            console.log(1);
+          }, 700);
+        }, 200);
+      }, 100);
+    }, 500);
+  }, 1000);
+}
