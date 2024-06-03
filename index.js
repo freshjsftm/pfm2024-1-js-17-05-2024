ed3f6cb98b450b50ccdafdf533b693fb40fb4fd1
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-06-03 09:34:24 +0300

    Add loadImage with Promise

--- index.js
+++ index.js
@@ -1,29 +1,26 @@
 'use strict';
 
-// function executor(resolve, reject) {
-//   (Math.random()>0.5) ? resolve() : reject();
-// }
-// const myPromise = new Promise(executor);
-// console.log(myPromise);
-
-const myPromise = new Promise((resolve, reject) => {
-  Math.random() > 0.5 ? resolve() : reject();
-});
-console.log(myPromise);
-
-//setTimeout(callback, time) -> timeout(time).then(callback)
-
-function timeout(time = 100) {
+function loadImage(path = '', alt = '', w = 100, h = 100) {
+  const img = new Image(w, h); //document.createElement('img')
+  img.src = path; // img.setAttribute('src', path);
+  img.alt = alt;  // img.setAttribute('alt', alt);
+  img.style.setProperty('object-fit', 'cover');
   return new Promise((resolve, reject) => {
-    setTimeout(() => resolve('data from promise'), time);
-    //setTimeout(()=>reject(new Error('error message')), time);
+    img.addEventListener('load', () => {
+      resolve(img);
+    });
+    img.addEventListener('error', () => {
+      reject(new Error('path invalid'));
+    });
   });
 }
 
-timeout(500)
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+loadImage('https://i.natgeofe.com/k/cd784533-e5ef-439a-8167-2ba61b0a9a4b/wave_16x9.jpg?w=1200', 'sea', 600,400)
+.then((elem)=>{
+  document.getElementById('root').append(elem)
+})
+.catch((error)=>{
+  console.log(error.message);
+  document.getElementById('root').append('picture')
+})
+
