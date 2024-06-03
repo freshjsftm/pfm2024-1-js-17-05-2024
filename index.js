5ecf391f35a21e6ffdae49a4a15511088c59619c
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-06-03 09:43:55 +0300

    Add static method Promise

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
 'use strict';
 
-function loadImage(path = '', alt = '', w = 100, h = 100) {
-  const img = new Image(w, h); //document.createElement('img')
-  img.src = path; // img.setAttribute('src', path);
-  img.alt = alt;  // img.setAttribute('alt', alt);
-  img.style.setProperty('object-fit', 'cover');
-  return new Promise((resolve, reject) => {
-    img.addEventListener('load', () => {
-      resolve(img);
-    });
-    img.addEventListener('error', () => {
-      reject(new Error('path invalid'));
-    });
-  });
-}
+// наші данні
+const number = 77;
 
-loadImage('https://i.natgeofe.com/k/cd784533-e5ef-439a-8167-2ba61b0a9a4b/wave_16x9.jpg?w=1200', 'sea', 600,400)
-.then((elem)=>{
-  document.getElementById('root').append(elem)
-})
-.catch((error)=>{
-  console.log(error.message);
-  document.getElementById('root').append('picture')
-})
+//огортаємо в проміс данні, щоб передати функції
+const numberPromiseF = Promise.resolve(number);
+const numberPromiseR = Promise.reject('number error');
 
+// складна функція зі сторонньої бібіліотеки
+// яка працює лише з промісами!!!
+const handlerPromise = (promise) => {
+  return promise
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+handlerPromise(numberPromiseF);
+handlerPromise(numberPromiseR);
